44e28533935424564951f8420c8a9284b41ca4c2
Vidiskiu Fortino K <vidiskiu.fk@gmail.com>
2019-10-04 18:47:35 +0700

    bug fix to remake

--- Google_poster_rename.py
+++ Google_poster_rename.py
@@ -9,7 +9,7 @@ def checkASCII(i):
         return True
     elif i < 58 and i > 47:
         return True
-    elif i == 32 or i == 46:
+    elif i == 32 or i == 46 or i == 45:
         return True
     else:
         return False
@@ -21,9 +21,8 @@ for image in dirlist:
     try:
         keyword = '-unnapproved'
         newImage = image[:image.find(keyword)] + '.jpg'
+        newImage = ''.join([i if checkASCII(i) else '' for i in newImage])
         print(newImage)
-
-        newImage = ''.join([i if checkASCII(i) else '' for i in image])
         os.rename(os.path.join(dir + image), os.path.join(dir + newImage))
     except FileNotFoundError:
         keyword = '-unnapproved'
@@ -31,5 +30,5 @@ for image in dirlist:
         newImage = ''.join([i if checkASCII(i) else '' for i in newImage])
         print(newImage)
 
-        newImage = ''.join([i if checkASCII(i) else '' for i in image])
+        newImage = ''.join([i if checkASCII(i) else '' for i in newImage])
         os.rename(os.path.join(dir + image), os.path.join(dir + newImage))
