17f1bf5f063afe9d7ea7fb434c081a8d80300e30
Vidiskiu Fortino K <vidiskiu.fk@gmail.com>
2019-10-04 18:35:24 +0700

    poster rename, remember naming conventions in windows os

---
+++ Google_poster_rename.py
@@ -0,0 +1,35 @@
+import os
+
+def checkASCII(i):
+    i = ord(i)
+
+    if i < 123 and i > 96:
+        return True
+    elif i < 91 and i > 64:
+        return True
+    elif i < 58 and i > 47:
+        return True
+    elif i == 32 or i == 46:
+        return True
+    else:
+        return False
+
+dir = 'Posters2/'
+dirlist = os.listdir(dir)
+
+for image in dirlist:
+    try:
+        keyword = '-unnapproved'
+        newImage = image[:image.find(keyword)] + '.jpg'
+        print(newImage)
+
+        newImage = ''.join([i if checkASCII(i) else '' for i in image])
+        os.rename(os.path.join(dir + image), os.path.join(dir + newImage))
+    except FileNotFoundError:
+        keyword = '-unnapproved'
+        newImage = image[:image.find(keyword)] + '.jpg'
+        newImage = ''.join([i if checkASCII(i) else '' for i in newImage])
+        print(newImage)
+
+        newImage = ''.join([i if checkASCII(i) else '' for i in image])
+        os.rename(os.path.join(dir + image), os.path.join(dir + newImage))
